8ea53101f1f8b371fc38bae228f0701e144ee2ae
fdv1 <francois@pytek.io>
2022-04-07 10:00:35 +0200

    moved callback outside app scope

--- snippets/component_property_as_callback.py
+++ snippets/component_property_as_callback.py
@@ -2,10 +2,10 @@
 from reflect_antd import Button, message
 
 
+def on_click():
+    message.warning("Please do not press this button again!")
+    print(f"User disobeyed!")
+
+
 def app():
-
-    def on_click():
-        message.warning("Please do not press this button again!")
-        print(f"User disobeyed!")
-
     return Button("Do not press this button", onClick=on_click, type="danger")
