0b840aa626aa8cdbe6db19fca7c316f8d05e3a2b
fdv1 <francois@pytek.io>
2022-04-27 10:20:14 +0100

    avoid creating observable directly

--- snippets/input_callback.py
+++ snippets/input_callback.py
@@ -1,30 +1,18 @@
-"""Component property as a simple callable."""
-from reflect import make_observable
+"""Two input components kept in sync."""
 from reflect_antd import InputNumber, Space
 
-
-def farenheit_to_celsius(farenheit_value):
-    return int((farenheit_value - 32) * 5 / 9)
-
-
-def celsius_to_farenheit(celsius_value):
-    return int(celsius_value * 9 / 5 + 32)
+INITIAL_CELSIUS_VALUE = 100
 
 
 def app():
-    INITIAL_CELSIUS_VALUE = 100
-    celsius = make_observable(INITIAL_CELSIUS_VALUE)
-    farenheit = make_observable(celsius_to_farenheit(INITIAL_CELSIUS_VALUE))
-
-    return Space(
-        [
-            InputNumber(
-                onChange=lambda c: farenheit.set(celsius_to_farenheit(c)), value=celsius
-            ),
-            "Celsius",
-            InputNumber(
-                onChange=lambda f: celsius.set(farenheit_to_celsius(f)), value=farenheit
-            ),
-            "Farenheit",
-        ]
+    farenheit_to_celsius = lambda f: int((f - 32) * 5 / 9)
+    celsius_to_farenheit = lambda c: int(c * 9 / 5 + 32)
+    celsius = InputNumber(
+        defaultValue=INITIAL_CELSIUS_VALUE,
+        onChange=lambda c: farenheit.set(celsius_to_farenheit(c)),
     )
+    farenheit = InputNumber(
+        defaultValue=celsius_to_farenheit(INITIAL_CELSIUS_VALUE),
+        onChange=lambda f: celsius.set(farenheit_to_celsius(f)),
+    )
+    return Space([celsius, "°C", farenheit, "°F"])
