79bf513e73d23fc518c0e0324d709b3cb0b00396
fdv1 <francois@pytek.io>
2022-11-11 17:58:11 +0000

    minor fixes

--- snippets/component_property_as_formula.py
+++ snippets/component_property_as_formula.py
@@ -1,9 +1,8 @@
 """Component property as a formula."""
 import reflect_antd as antd
-import reflect_html as html
 
 
 def app():
     level = antd.Slider(min=1, max=5, defaultValue=1, key="level", style={"width": 100})
     title = antd.Typography.Title("Lorem Ipsum", level=level)
-    return html.div([level, title])
+    return antd.Space([level, title])
--- snippets/formula_as_lambda.py
+++ snippets/formula_as_lambda.py
@@ -1,10 +1,9 @@
 """Formula as a lambda expression"""
 import reflect_antd as antd
-import reflect_html as html
 
 
 def app():
     name = antd.Input(defaultValue="John")
     greeting = lambda: f"Hello {name()}"
-    greeting_component = html.div(greeting)
+    greeting_component = antd.TypographyText(greeting)
     return antd.Space([name, greeting_component])
